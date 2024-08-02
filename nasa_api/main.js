73478a9031c8f485255662f4fc60b2337b696f4c
umer islam <139012668+Umer-Islam@users.noreply.github.com>
2024-08-03 03:56:39 +0500

    working mvp

--- nasa_api/main.js
+++ nasa_api/main.js
@@ -1,17 +1,22 @@
-require('dotenv').config()
-
 let h1 = document.querySelector("h1");
-let descrption = document.querySelector('#description')
+let h2Title = document.querySelector("h2");
+let descrption = document.querySelector("#description");
 let now = new Date("2024-01-02T00:00:00Z");
+let image = document.querySelector("img");
+let video = document.querySelector("iframe");
 
 h1.textContent = `Nasa api daily post: ${now}`;
 
- const apiKey=process.env.NASA_API_KEY;
-url = "https://api.nasa.gov/planetary/apod?api_key=${process.env.NASA_API_KEY}";
+url =
+  "https://api.nasa.gov/planetary/apod?api_key=SWollBpigW0V9Ax1SHhgMsydLHSCV8AfftI3px3m";
 fetch(url)
   .then((res) => res.json())
   .then((data) => {
     console.log(data);
-    descrption.textContent = `${data.explanation}`
+    descrption.textContent = `${data.explanation}`;
+
+    h2Title.textContent = `${data.title}`;
+    console.log(`${data.url}`)
+    image.src = `${data.url}`;
   })
   .catch((err) => console.log(`error: ${err}`));
